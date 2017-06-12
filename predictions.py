3e698f93c085888173d8c499e788a8ccb7c10d40
Anshumaan Parashar <apairs.anshumaan@gmail.com>
2017-06-13 00:47:44 +0530

    Initial Commit

---
+++ predictions.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import datapreprocessing as dp
+import Classification_methods as clam
+import Regression_methods as reg
+import Clustering_methods as clum
+import metrics
+
+
+# Make your model here
