a111dd0fd84b430b7c5a9ea0c0b8ad0de75c4436
Anshumaan Parashar <apairs.anshumaan@gmail.com>
2017-07-18 01:50:03 +0530

    Change impute method to impute categorical data also

--- datapreprocessing.py
+++ datapreprocessing.py
@@ -2,12 +2,28 @@ from sklearn.preprocessing import Imputer
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.cross_validation import train_test_split
+import pandas as pd
+import numpy as np
+
+from sklearn.base import TransformerMixin
 
 
-def impute(X, list_indices):
+class DataFrameImputer(TransformerMixin):
+
+    def __init__(self):
+
+    def fit(self, X, y=None, ):
+        self.fill = pd.Series([X[c].value_counts().index[0]
+            if X[c].dtype == np.dtype('O') else X[c].mean() for c in X],
+            index=X.columns)
+        return self
+
+    def transform(self, X, y=None):
+        return X.fillna(self.fill)
+
+def impute(X):
     print('Providing missing values..')
-    imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
-    X[:, list_indices] = imputer.fit_transform(X[:, list_indices])
+    X = DataFrameImputer().fit_transform(X)
     return X
 
 
@@ -39,3 +55,5 @@ def scaling(X):
     sc = StandardScaler()
     X = sc.fit_transform(X)
     return X
+
+
